d443491b0321d37ebdccf6866cb81b2e0bcb43f1
Ashish's ASUS laptop <hrushikeshmohanty90@gmail.com>
2022-12-10 16:07:21 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express();
+const cors = require("cors")
+app.use(cors())
+app.use(express.json())
+
+const PORT = 9000
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json())
+
+//default route
+app.get("/", async (request, response) => {
+    await fetch(`https://dzsqyl-8080.preview.csb.app/patient`)
+    .then(res => {console.log(res); response.json({data: res})}).catch(err => response.json({err}))
+})
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log(`listening on port ${PORT}`)
+    }
+})
